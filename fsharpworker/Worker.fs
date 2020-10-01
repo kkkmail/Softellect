884449209f7cd74cb0ff74a0445e82c053e14072
kkkmail <kkk@jfcglobal.net>
2020-10-01 08:55:00 -0400

    20201001

---
+++ fsharpworker/Worker.fs
@@ -0,0 +1,37 @@
+namespace fsharpworker
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading
+open System.Threading.Tasks
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+type Worker(logger: ILogger<Worker>) =
+    inherit BackgroundService()
+
+    override _.StartAsync(ct: CancellationToken) =
+        async {
+                do! Async.Sleep(0)
+        }
+        |> Async.StartAsTask
+        :> Task
+
+
+    override _.StopAsync(ct: CancellationToken) =
+        async {
+                do! Async.Sleep(0)
+        }
+        |> Async.StartAsTask
+        :> Task
+
+
+    override _.ExecuteAsync(ct: CancellationToken) =
+        async {
+            while not ct.IsCancellationRequested do
+                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now)
+                do! Async.Sleep(1000)
+        }
+        |> Async.StartAsTask
+        :> Task // need to convert into the parameter-less task
