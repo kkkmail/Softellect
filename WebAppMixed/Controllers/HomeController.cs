fa9e8efb66d368f4266dd3960cacead06ef794d3
kkkmail <kkk@jfcglobal.net>
2020-10-16 12:41:35 -0400

    0.0.1-alpha.100

--- WebAppMixed/Controllers/HomeController.cs
+++ WebAppMixed/Controllers/HomeController.cs
@@ -1,4 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿#if NETFRAMEWORK480
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppNetFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
+
+#else
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -35,3 +69,4 @@ namespace WebAppMvc.Controllers
         }
     }
 }
+#endif
