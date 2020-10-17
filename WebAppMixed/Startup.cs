072ea9966c0f9a563149880bf832824275da34c5
kkkmail <kkk@jfcglobal.net>
2020-10-17 18:00:07 -0400

    0.0.1-alpha.100

--- WebAppMixed/Startup.cs
+++ WebAppMixed/Startup.cs
@@ -15,24 +15,6 @@ namespace WebAppMvc
 {
     public class Startup
     {
-        /// <summary>
-        /// https://weblog.west-wind.com/posts/2018/Apr/12/Getting-the-NET-Core-Runtime-Version-in-a-Running-Application
-        /// </summary>
-        public static string GetNetVersion()
-        {
-            var framework =
-                //Environment.Version.ToString();
-
-                System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
-
-                //Assembly
-                //    .GetEntryAssembly()?
-                //    .GetCustomAttribute<TargetFrameworkAttribute>()?
-                //    .FrameworkName;
-
-            return framework;
-        }
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
