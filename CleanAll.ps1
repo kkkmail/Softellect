66348d9726620454523ae6834499a8ea3bc9d370
kkkmail <kkk@jfcglobal.net>
2023-11-19 14:07:33 -0500

    NET 8.0

--- CleanAll.ps1
+++ CleanAll.ps1
@@ -15,60 +15,32 @@ if ($w3wp) { Stop-Process -InputObject $w3wp -force }
 Get-Process | Where-Object {$_.HasExited}
 
 if ((!$msb) -and (!$vbcsc) -and (!$w3wp)) { echo "No known orphanded processes found!" }
-                                          
 
 echo "Deleting all bin and obj content..."
+$paths = "."
 
-Remove-Item -path .\NugetPackages\*.nupkg -force -ea silentlycontinue
+foreach ($path in $paths)
+{
+    $directories = Get-ChildItem -Path $path -Directory -Recurse
 
-Remove-Item -path .\Data\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Interop\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Messaging\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\MessagingData\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Platform\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgClientOne\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgClientTwo\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgService\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgServiceinfo\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgWorker\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\NetCoreClient\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\NetCoreService\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfClient\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfService\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfServiceInfo\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfWorker\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfWorker2\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WebWorker\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\ServiceProxy\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Sys\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\TestUpdateJson\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\TestUpdateJson2\bin -recurse -force -ea silentlycontinue
-Remove-Item -path .\Wcf\bin -recurse -force -ea silentlycontinue
+    foreach ($directory in $directories)
+    {
+        $binFolder = Join-Path -Path $directory.FullName -ChildPath "bin"
+        $objFolder = Join-Path -Path $directory.FullName -ChildPath "obj"
 
-Remove-Item -path .\Data\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Interop\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Messaging\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\MessagingData\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Platform\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgClientOne\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgClientTwo\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgService\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgServiceinfo\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Msg\MsgWorker\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\NetCoreClient\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\NetCoreService\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfClient\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfService\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfServiceInfo\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfWorker\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WcfWorker2\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Samples\Wcf\WebWorker\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\ServiceProxy\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Sys\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\TestUpdateJson\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\TestUpdateJson2\obj -recurse -force -ea silentlycontinue
-Remove-Item -path .\Wcf\obj -recurse -force -ea silentlycontinue
+        if (Test-Path -Path $binFolder)
+        {
+            Write-Output "Deleting folder: $binFolder"
+            Remove-Item -Path $binFolder -Recurse -ErrorAction SilentlyContinue -Force
+        }
 
+        if (Test-Path -Path $objFolder)
+        {
+            Write-Output "Deleting folder: $objFolder"
+            Remove-Item -Path $objFolder -Recurse -ErrorAction SilentlyContinue -Force
+        }
+    }
+}
 
 echo "Deleting all garbage from user Temp folder..."
 Remove-Item -path $env:userprofile\AppData\Local\Temp -recurse -force -ea silentlycontinue
