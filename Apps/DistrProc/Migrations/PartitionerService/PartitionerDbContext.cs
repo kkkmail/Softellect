042fd793c09dd33a43ba9adc584f06f5fcf238df
kkkmail <kkk@jfcglobal.net>
2025-05-05 18:41:50 -0400

    9.0.202.24

---
+++ Apps/DistrProc/Migrations/PartitionerService/PartitionerDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Softellect.Migrations.Common;
+
+namespace Softellect.Migrations.PartitionerService;
+
+public class PartitionerDbContext : CommonDbContext<PartitionerDbContext>, IHasServiceName
+{
+    public static string GetServiceName() => "PartitionerService";
+
+    public PartitionerDbContext() : base(GetDesignTimeOptions())
+    {
+    }
+
+    public PartitionerDbContext(DbContextOptions<PartitionerDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<DeliveryType> DeliveryTypes { get; set; } = null!;
+    public DbSet<Message> Messages { get; set; } = null!;
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+
+        // Set decimal precision scale decimal(38, 16) as a default
+        configurationBuilder
+            .Properties<decimal>()
+            .HavePrecision(38, 16);
+    }
+}
