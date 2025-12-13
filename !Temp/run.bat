94953f1de207a416fee2c4d6d73dca134f68b284
Konstantin Konstantinov <kkkmail@usa.net>
2025-12-13 16:45:48 -0500

    9.0.300.x

--- !Temp/run.bat
+++ !Temp/run.bat
@@ -2,7 +2,7 @@ pktmon stop
 pktmon reset
 pktmon filter remove
 pktmon filter add -p 53
-pktmon start --etw -p 0 -c 1 --file-name C:\GitHub\Softellect\!Temp\vpn_dns.etl
+pktmon start --etw -c 200 --file-name C:\GitHub\Softellect\!Temp\vpn_dns.etl
 nslookup -timeout=10 dns.msftncsi.com 10.66.77.1
 pktmon stop
 pktmon pcapng C:\GitHub\Softellect\!Temp\vpn_dns.etl -o C:\GitHub\Softellect\!Temp\vpn_dns.pcapng
