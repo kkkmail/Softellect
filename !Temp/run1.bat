38f02b821cad4a23dff94c94ca7813e092d29009
Konstantin Konstantinov <kkkmail@usa.net>
2026-01-16 10:57:25 -0500

    10.0.100.x.Linux

--- !Temp/run1.bat
+++ !Temp/run1.bat
@@ -1,7 +1,8 @@
 pktmon stop
 pktmon reset
 pktmon filter remove
-pktmon filter add -p 47291
+# pktmon filter add -p 47291
+pktmon filter add -p 45001
 pktmon start --etw -m real-time --file-name C:\GitHub\Softellect\!Temp\vpn.etl
 # pktmon stop
 # pktmon pcapng C:\GitHub\Softellect\!Temp\vpn.etl -o C:\GitHub\Softellect\!Temp\vpn.pcapng
